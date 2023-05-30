b8852b5caa14258e948c7da7f309ddab8a32de9d
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-05-30 14:25:51 +0100

    Solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let temp = (fTemp - 32) / 1.8;
+  return Number(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let temp = cTemp*1.8 + 32;
+  return Number(temp.toFixed(1));
 };
+
+console.log(convertToCelsius(90))
 
 // Do not edit below this line
 module.exports = {
