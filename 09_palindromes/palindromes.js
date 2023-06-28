6f8c1238b53550a7e1a7d379d62ce3d7edc8728c
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-06-28 16:25:01 +0100

    Palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^\w]/g, "");
+  const stringArray = string.split("");
+  const reverseArray = stringArray.reverse();
+  const reverseString = reverseArray.join("");
+  console.log(reverseString);
 
+  if (reverseString === string) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes("Lid off a daffodil"));
 
 // Do not edit below this line
 module.exports = palindromes;
