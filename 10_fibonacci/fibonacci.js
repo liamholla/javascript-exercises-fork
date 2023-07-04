189c3bd01c15c0768d647a8e2f982bf8e7416734
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-07-04 11:54:57 +0100

    Solutions to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let fib = [1];
 
+  // Need separate rules if the user asks for 1st of 2nd Fib number
+  if (number < 0) {
+    return "OOPS";
+  } else if (number === 1) {
+    return fib;
+  } else if (number === 2) {
+    fib = [1, 1];
+    return fib;
+  } else {
+    fib = [1, 1];
+    for (i = 2; i < number; i++) {
+      fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    console.log(fib);
+    value = fib[number - 1];
+    return value;
+  }
 };
+
+console.log(fibonacci(20));
 
 // Do not edit below this line
 module.exports = fibonacci;
